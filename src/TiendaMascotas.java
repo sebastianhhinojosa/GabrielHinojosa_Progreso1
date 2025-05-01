e820f895efc655aa1aa9218037a835a536f2fce6
sebastianhhinojosa <gabriel.hinojosa@udla.edu.ec>
2025-05-01 17:19:20 -0500

    Commit

--- src/TiendaMascotas.java
+++ src/TiendaMascotas.java
@@ -11,4 +11,21 @@ public class TiendaMascotas {
         especie4 = new Especie("Guacamaya Roja", Especie.Categoria.ave,3,50,20);
 
     }
+
+    public Especie getEspecie1() {
+        return especie1;
+    }
+
+    public Especie getEspecie2() {
+        return especie2;
+    }
+
+    public Especie getEspecie3() {
+        return especie3;
+    }
+
+    public Especie getEspecie4() {
+        return especie4;
+    }
+
 }
